89d32a522c5bbdd0ed5e3a9957542131ae6f9ac7
Emetib <theemetib@gmail.com>
2013-03-02 13:12:37 +0000

    cube v1.3
    3d cube with working swipe controls.

--- cubejs.js
+++ cubejs.js
@@ -33,26 +33,27 @@
 				
 			//Swipe Coding for Mobile Device.
 			//Thanks to dmi3y (rhythmcode.com) @ StackOverflow (http://stackoverflow.com/questions/13726196/how-to-swipe-between-several-div-in-one-single-jquery-mobile-pages)
-				$( document ).bind('swipe', function (d) {
-   
-            switch (d) {
-                case 'r': // swipe right
-                    console.log('swipe right');
-					yAngle += 90;
-                    break;
-                case 'l': // swipe left
-                    console.log('swipe left');
-					yAngle -= 90;
-                    break;
-                case 'u': // swipe up
-                    console.log('swipe up');
-					xAngle += 90;
-                    break;
-                case 'd': // swipe down
-                    console.log('swipe down');
-					xAngle -= 90;
-                    break;
-               }
+			//for insperation. Adapted later to include jGesture library support.
+				$( document ).bind('swipeup swipedown swipeleft swiperight', function (ev) {
+   switch (ev.type) {
+        case 'swiperight': // swipe right
+            console.log('swipe right');
+			yAngle += 90;
+            break;
+        case 'swipeleft': // swipe left
+            console.log('swipe left');
+			yAngle -= 90;
+            break;
+        case 'swipeup': // swipe up
+            console.log('swipe up');
+			xAngle += 90;
+            break;
+        case 'swipedown': // swipe down
+            console.log('swipe down');
+			xAngle -= 90;
+            break;
+					   
+			   }
 	document.getElementById('cube').style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)"; 
 });
 				
